26f9d51f66e8a35cfdd4b37c44a220b408b6bbb4
Brunitoz <o.bruno28@escola.pr.gov.br>
2025-11-06 11:30:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Espera o documento carregar completamente
+document.addEventListener('DOMContentLoaded', () => {
 
+    // Seleciona o botão pelo ID que demos a ele no HTML
+    const botao = document.getElementById('botao-contato');
+
+    // Adiciona um "ouvinte" de clique no botão
+    botao.addEventListener('click', () => {
+        // Ação que acontece ao clicar
+        alert('Meu email é: seu-email@exemplo.com');
+    });
+
+});
